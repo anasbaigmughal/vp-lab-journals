863f54b457354fb87fcc469d7a938cffb67ab52e
Daniya Sehrish <anasbaigmughal@gmail.com>
2020-07-02 12:07:32 +0500

    added ex2

---
+++ Lab6/Lab6-Ex2/Lab6-Ex2/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Lab6_Ex2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.carsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.audiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mercedesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toyotaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hondaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.carsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(547, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // carsToolStripMenuItem
+            // 
+            this.carsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.audiToolStripMenuItem,
+            this.mercedesToolStripMenuItem,
+            this.toyotaToolStripMenuItem,
+            this.hondaToolStripMenuItem});
+            this.carsToolStripMenuItem.Name = "carsToolStripMenuItem";
+            this.carsToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.carsToolStripMenuItem.Text = "Cars";
+            // 
+            // audiToolStripMenuItem
+            // 
+            this.audiToolStripMenuItem.Name = "audiToolStripMenuItem";
+            this.audiToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.audiToolStripMenuItem.Text = "Audi";
+            this.audiToolStripMenuItem.Click += new System.EventHandler(this.audiToolStripMenuItem_Click);
+            // 
+            // mercedesToolStripMenuItem
+            // 
+            this.mercedesToolStripMenuItem.Name = "mercedesToolStripMenuItem";
+            this.mercedesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.mercedesToolStripMenuItem.Text = "Mercedes";
+            this.mercedesToolStripMenuItem.Click += new System.EventHandler(this.mercedesToolStripMenuItem_Click);
+            // 
+            // toyotaToolStripMenuItem
+            // 
+            this.toyotaToolStripMenuItem.Name = "toyotaToolStripMenuItem";
+            this.toyotaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.toyotaToolStripMenuItem.Text = "Toyota";
+            this.toyotaToolStripMenuItem.Click += new System.EventHandler(this.toyotaToolStripMenuItem_Click);
+            // 
+            // hondaToolStripMenuItem
+            // 
+            this.hondaToolStripMenuItem.Name = "hondaToolStripMenuItem";
+            this.hondaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.hondaToolStripMenuItem.Text = "Honda";
+            this.hondaToolStripMenuItem.Click += new System.EventHandler(this.hondaToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 400);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Model:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Brand:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(290, 399);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Colour:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(290, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Engine Power:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(124, 400);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(124, 428);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 20);
+            this.label6.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(420, 399);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 20);
+            this.label7.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(420, 428);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 20);
+            this.label8.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(38, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(472, 333);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 475);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem carsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem audiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mercedesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toyotaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hondaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Lab6/Lab6-Ex2/Lab6-Ex2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab6_Ex2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void audiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "C:/Users/MABM/Desktop/1.png";
+            label5.Text = "2016"; //model
+            label6.Text = "Audi"; //brand
+            label7.Text = "Black"; //colour
+            label8.Text = "2500cc"; //engine power
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mercedesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "C:/Users/MABM/Desktop/2.png";
+            label5.Text = "2015"; //model
+            label6.Text = "Mercedes"; //brand
+            label7.Text = "Black"; //colour
+            label8.Text = "3000cc"; //engine power
+        }
+
+        private void toyotaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "C:/Users/MABM/Desktop/3.png";
+            label5.Text = "2017"; //model
+            label6.Text = "Toyota"; //brand
+            label7.Text = "Red"; //colour
+            label8.Text = "3000cc"; //engine power
+        }
+
+        private void hondaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "C:/Users/MABM/Desktop/4.png";
+            label5.Text = "2012"; //model
+            label6.Text = "Honda"; //brand
+            label7.Text = "Black"; //colour
+            label8.Text = "3500cc"; //engine power
+        }
+    }
+}
